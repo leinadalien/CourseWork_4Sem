0e3f8be4dbbb1a968e4c4bebd0545b4f8c321ede
leinadalien <danila.sokolyuk@gmail.com>
2022-05-23 22:57:26 +0300

    first

--- CourseWork/Game.cs
+++ CourseWork/Game.cs
@@ -3,40 +3,30 @@ using SFML.Window;
 
 namespace CourseWork
 {
-    internal class Game
+    public class Game
     {
-        private World world;
-        Clock clock;
+        private GameSettings settings = new GameSettings()
+        {
+            AspectRatio = (16, 9),
+            TileSize = 32,
+        };
+        public GameSettings Settings { get { return settings; } set { settings = value; } }
         public Game()
         {
-            player = new();
-            world = new(2)
-            {
-                Player = player
-            };
-            player.SetLocation(world.Locations.First());
-            player.Spawn();
-            clock = new Clock();
-            //location.PrintMap();
+            
         }
-        public void Update()
+        void Update()
         {
-            player.Movement.X = Keyboard.IsKeyPressed(Keyboard.Key.A) ? -player.MovementSpeed : Keyboard.IsKeyPressed(Keyboard.Key.D) ? player.MovementSpeed : 0;
-            player.Movement.Y = Location.WORlD_COMPRESSION_Y * (Keyboard.IsKeyPressed(Keyboard.Key.W) ? -player.MovementSpeed : Keyboard.IsKeyPressed(Keyboard.Key.S) ? player.MovementSpeed : 0);
-            player.IsTested = Keyboard.IsKeyPressed(Keyboard.Key.T);
-            int deltaTime = clock.ElapsedTime.AsMilliseconds();
-            player.Update(deltaTime);
-           // locations.First().Update();
-            //locations[1].Update();
-            clock.Restart();
-            Program.Window.Draw(world);
-            //Game stats
-            Console.SetCursorPosition(0, 0);
-            Console.WriteLine($"Player position: X({(int)player.Position.X}) Y({(int)player.Position.Y}) [{(int)player.Position.X / Tile.TILE_SIZE}][{(int)(player.Position.Y / Tile.TILE_SIZE / Location.WORlD_COMPRESSION_Y)}]          ");
-            Console.WriteLine($"Camera position: X({-(int)player.location.Position.X}) Y({-(int)player.location.Position.Y})      ");
-            Console.WriteLine($"Camera X: [{0 - (int)player.location.Position.X / Tile.TILE_SIZE},{Program.Window.Size.X / Tile.TILE_SIZE + 1 - (int)player.location.Position.X / Tile.TILE_SIZE}]    ");
-            Console.WriteLine($"Camera Y: [{0 - (int)(player.location.Position.Y / Tile.TILE_SIZE / Location.WORlD_COMPRESSION_Y)},{Program.Window.Size.Y / Tile.TILE_SIZE / Location.WORlD_COMPRESSION_Y + 1 - (int)(player.location.Position.Y / Tile.TILE_SIZE / Location.WORlD_COMPRESSION_Y)}]    ");
-           // Console.WriteLine((int)(1000/deltaTime) + "fps  ");
+
         }
+        void Draw()
+        {
+
+        }
+    }
+    public struct GameSettings
+    {
+        public (int, int) AspectRatio { get; set; }
+        public int TileSize { get; set; }
     }
 }
