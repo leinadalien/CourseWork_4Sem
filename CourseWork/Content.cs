f80313cd649d7e59b1d6207a9dcbb0e73f450b04
leinadalien <danila.sokolyuk@gmail.com>
2022-06-06 03:48:00 +0300

    finaly

--- CourseWork/Content.cs
+++ CourseWork/Content.cs
@@ -2,9 +2,9 @@
 
 namespace CourseWork
 {
-   public class Content
+    public class Content
     {
-        public const string DIRECTORY = "../Content/";
+        public static string DIRECTORY = Directory.GetCurrentDirectory() + "/Content/";
         public static Texture TilesTexture = new(DIRECTORY + "tiles.jpg");
         public static Texture StonesTexture = new(DIRECTORY + "stones.png");
         public static Texture HighTreesTexture = new(DIRECTORY + "high_trees.png");
